361fb71894069f226f94046edc6aba4a6f9edfc1
HadleyLiao <154941260+hadleyliao@users.noreply.github.com>
2025-07-22 14:35:52 +0800

    Initial Commit

---
+++ Keyboard/KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardApp.swift
+//  Keyboard
+//
+//  Created by 訪客使用者 on 2025/7/22.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeyboardTests/KeyboardTests.swift
@@ -0,0 +1,16 @@
+//
+//  KeyboardTests.swift
+//  KeyboardTests
+//
+//  Created by 訪客使用者 on 2025/7/22.
+//
+
+import Testing
+
+struct KeyboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
